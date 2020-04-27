b7597e9b1f2d54b9f5bcb17adf22288956c28896
viperx77 <lindell.mark@gmail.com>
2020-04-27 09:09:32 -0400

    fix build

--- build.ps1
+++ build.ps1
@@ -9,7 +9,7 @@ Write-Verbose "Setting version to $VERSION"
 ((Get-Content -path hsdp-iam-template.nuspec -Raw) -replace '\${NUGET_VERSION}',$VERSION) | Set-Content -Path hsdp-iam.nuspec
 ((Get-Content -path hsdp-iam-template.psd1 -Raw) -replace '\${NUGET_VERSION}',$VERSION) | Set-Content -Path hsdp-iam.psd1
 
-$osEnv = (Get-ChildItem -Path Env:OS)
+$osEnv = (Get-ChildItem -Path ENV: | Where-Object { $_.name -eq 'OS'})
 if ($osEnv -and $osEnv.Value -eq "Windows_NT" ) {
     & nuget pack hsdp-iam.nuspec -NoPackageAnalysis -OutputDirectory $PSScriptRoot/target
 } else {
