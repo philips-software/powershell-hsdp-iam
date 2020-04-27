d8584436db174500cddea8d7b62cd83a7fca60ee
viperx77 <lindell.mark@gmail.com>
2020-04-27 09:22:20 -0400

    fix build

--- build.ps1
+++ build.ps1
@@ -10,10 +10,11 @@ Write-Verbose "Setting version to $VERSION"
 ((Get-Content -path hsdp-iam-template.psd1 -Raw) -replace '\${NUGET_VERSION}',$VERSION) | Set-Content -Path hsdp-iam.psd1
 
 $osEnv = (Get-ChildItem -Path ENV: | Where-Object { $_.name -eq 'OS'})
+Write-Verbose $osEnv
 if ($osEnv -and $osEnv.Value -eq "Windows_NT" ) {
     & nuget pack hsdp-iam.nuspec -NoPackageAnalysis -OutputDirectory $PSScriptRoot/target
 } else {
-    & mono $NUGET_EXE pack hsdp-iam.nuspec -NoPackageAnalysis -OutputDirectory $PSScriptRoot/target
+    & mono pack hsdp-iam.nuspec -NoPackageAnalysis -OutputDirectory $PSScriptRoot/target
 }
 
 Pop-Location
