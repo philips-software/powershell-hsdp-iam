b96e2c3b9c9b8ba9b3c595a025dbbd2195246fed
viperx77 <lindell.mark@gmail.com>
2020-04-27 08:49:24 -0400

    fixed tests

--- hsdp-iam.tests.ps1
+++ hsdp-iam.tests.ps1
@@ -1,5 +1,10 @@
+$VerbosePreference = "continue"
+Push-Location $PSScriptRoot/src
+
 if ((Get-Module -list -Name Pester) -notcontains 'pester') {
     Install-Module -Name Pester -SkipPublisherCheck -RequiredVersion 4.9.0 -Force
 }
 
 Invoke-Pester -ExcludeTag 'Disabled' -Script @{ Path = 'Public/*' } -OutputFile "./test-pester.xml" -OutputFormat 'NUnitXML'
+
+Pop-Location
