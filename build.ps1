d9be3ff4c8f2d7613a792dc6f46e10e361a67f92
viperx77 <lindell.mark@gmail.com>
2020-04-27 09:56:00 -0400

    fix

--- build.ps1
+++ build.ps1
@@ -1,17 +1,18 @@
-param($branchName)
-
-Write-Verbose "Branch Name: $($branchName)"
+param($branchName, $semver)
 
 $VerbosePreference = "continue"
+
 Push-Location $PSScriptRoot/src
+
+Write-Verbose "branch: $($branchName)"
+Write-Verbose "semver: $($semver)"
 
 # ensure the module imports
 Import-Module -Name ./hsdp-iam -Force
 
-$VERSION = "99.99.99"
-Write-Verbose "Setting version to $VERSION"
-((Get-Content -path hsdp-iam-template.nuspec -Raw) -replace '\${NUGET_VERSION}',$VERSION) | Set-Content -Path hsdp-iam.nuspec
-((Get-Content -path hsdp-iam-template.psd1 -Raw) -replace '\${NUGET_VERSION}',$VERSION) | Set-Content -Path hsdp-iam.psd1
+Write-Verbose "Setting version"
+((Get-Content -path hsdp-iam-template.nuspec -Raw) -replace '\${NUGET_VERSION}',$semver) | Set-Content -Path hsdp-iam.nuspec
+((Get-Content -path hsdp-iam-template.psd1 -Raw) -replace '\${NUGET_VERSION}',$semver) | Set-Content -Path hsdp-iam.psd1
 
 $osEnv = (Get-ChildItem -Path ENV: | Where-Object { $_.name -eq 'OS'})
 if ($osEnv -and $osEnv.Value -eq "Windows_NT" ) {
