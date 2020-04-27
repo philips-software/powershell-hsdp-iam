d90ec010b17cb7b870861441842a906417fe57a8
Mark Lindell <50016830+marklindell@users.noreply.github.com>
2020-04-27 08:06:17 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 $VerbosePreference = "continue"
 ((Get-Content -path ./hsdp-iam.nuspec -Raw) -replace '\${NUGET_VERSION}','99.99.99') | Set-Content -Path ./hsdp-iam.nuspec
 ((Get-Content -path ./hsdp-iam.psd1 -Raw) -replace '\${NUGET_VERSION}','99.99.99') | Set-Content -Path ./hsdp-iam.psd1
-& nuget pack -NoPackageAnalysis
+& mono $NUGET_EXE pack -NoPackageAnalysis
 import-module -Name ./hsdp-iam -Force
